82d340a3f35f7cb8ab41cbaaa236b7e11614709b
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-08-03 20:43:21 -0300

    Counter reduced to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-import { setupCounter } from './counter.js';
+//import { setupCounter } from './counter.js';
+export function setupCounter(element) {
+  let counter = 0;
+  const setCounter = (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener('click', () => setCounter(++counter));
+  setCounter(0);
+}
 
 setupCounter(document.querySelector('#counter'));

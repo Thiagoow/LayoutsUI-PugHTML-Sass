2de65cb55916641e3350cc210fb4a1af1804e514
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-08-19 19:27:08 -0300

    Creating photoMosaic page

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ const PugPlugin = require('pug-plugin');
 module.exports = {
   entry: {
     index: './src/views/home.pug',
-    manyInOne: './src/views/manyInOne.pug'
-    /*     
-    photoMosaic: './src/views/photoMosaic.pug', 
-    masonryLayout: './src/views/masonryLayout.pug' */
+    manyInOne: './src/views/manyInOne.pug',
+    photoMosaic: './src/views/photoMosaic.pug'
+    //masonryLayout: './src/views/masonryLayout.pug'
   },
   output: {
     path: path.join(__dirname, 'dist/'),
